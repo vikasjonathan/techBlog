459dd0624ec80d997a5005b0c155f79c4049ddde
Vikas David Jonathan <vikas.jonathan@gmail.com>
2024-02-16 14:17:05 -0600

    added logout.js

---
+++ public/js/logout.js
@@ -0,0 +1,17 @@
+// with the help of mini project
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  };
+  
+  document.querySelector('#logout').addEventListener('click', logout);
+  
+  
