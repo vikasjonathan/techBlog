d60a16b8245f92e9a983a53822030de456c6e489
Vikas David Jonathan <vikas.jonathan@gmail.com>
2024-02-16 12:44:09 -0600

    updated index.js in models

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,7 +4,7 @@ const { Comment } = require('../../models');
 //post route to create a comment
 router.post('/', async (req, res) => {
     try {
-      const commentData = await Comment.create({
+      const commentData = await Comments.create({
         content: req.body.content,
         user_id: req.session.user_id,
         post_id: req.body.post_id,
--- models/index.js
+++ models/index.js
@@ -1,14 +1,32 @@
 const User = require('./User');
 const BlogPost = require('./BlogPost');
+const Comments = require('./Comments')
+
 User.hasMany(BlogPost, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-  });
-  
-  BlogPost.belongsTo(User, {
-    foreignKey: 'user_id'
-  });
-  
-  module.exports = { User, BlogPost };
-  
-  
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+BlogPost.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+User.hasMany(Comments, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comments.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+BlogPost.hasMany(Comments, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(BlogPost, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, BlogPost, Comments };
+
