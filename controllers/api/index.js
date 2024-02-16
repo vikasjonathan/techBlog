6a8f9fbcac3b68e6428f9c9a10833093e54b499f
Vikas David Jonathan <vikas.jonathan@gmail.com>
2024-02-16 12:23:06 -0600

    Added dependieces in index for the userroute

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@
 const router = require('express').Router();
 const blogpostRoutes = require('./blogpostRoutes');
 const commentRoutes = require('./commentRoutes');
+const userRoutes = require('./userRoutes');
 
 router.use('/blogpost', blogpostRoutes);
 router.use('/comment', commentRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
